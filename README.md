ff001782c0f836804defa726c33d50d2ef36254b
arnoandaivengo <alexobukhov@hotmail.com>
2020-02-13 16:18:42 +0300

    searchUser and searchImage resolver

--- README.md
+++ README.md
@@ -2,16 +2,15 @@
 
 Instragram clone with Express + Prisma + React and React Native
 
-
 ## User Stories
 
 - [x] Create account
-- [ ] Request Secret
-- [ ] Confirm Secret (Login)
-- [ ] Log in
-- [ ] Like / Unlike a photo
-- [ ] Comment on a photo
-- [ ] Search by user
+- [x] Request Secret
+- [x] Confirm Secret (Login)
+- [x] Like / Unlike a photo
+- [x] Comment on a photo
+- [x] Log in
+- [x] Search by user
 - [ ] Search by location
 - [ ] See user profile
 - [ ] Follow / Unfollow User
--- src/server.js
+++ src/server.js
@@ -1,26 +1,18 @@
-require("dotenv").config()
+import "./env"
 import { GraphQLServer } from "graphql-yoga"
 import logger from "morgan"
-
 import schema from "./schema"
+import "./passport"
+import { authenticateJwt } from "./passport"
 
 const PORT = process.env.PORT || 4001
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`
-
-
-const resolvers = {
-    Query: {
-        hello: () => "hi"
-    }
-}
-
-const server = new GraphQLServer({ schema })
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({ request })
+})
 
 server.express.use(logger("dev"))
+server.express.use(authenticateJwt)
 
-server.start({port: PORT}, () => console.log(`Server running on http://localhost:${PORT}`))
+server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`))
